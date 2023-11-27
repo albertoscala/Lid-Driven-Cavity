7a73121604a189f992df9a7972dfc2172b2477e9
Alberto Scala <alby.scala@gmail.com>
2023-11-27 23:55:01 +0100

    readmea updated

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Lid-Driven-Cavity
+
+This repository serves as a comprehensive documentation of my journey to realise my first CFD (Computational Fluid Dynamics). Throughout this repository, I will meticulously detail the step-by-step process that led me to grasp the fundamentals of CFD. Commencing with a focus on mathematical foundations, I will delve into computations and problem-solving techniques. The ultimate aim is to progress steadily towards the application of CFD, culminating in the simulation of a Lid Driven Cavity.
+
+## Table of Contents
+
+- [Mathematical Foundations](#Mathematical-Foundations)
+  - [Differential Equations](#Differential-Equations)
+  - [Finite Difference Method](#Finite-Difference-Method)
+  - [Linear Algebra](#Linear-Algebra)
+  - [Matrix Decomposition](#Matrix-Decomposition)
+  - [Iterative Methods](#Iterative-Methods)
+- [Computations](#Computations)
+  - [Boundary Conditions](#Boundary-Conditions)
+  - [Discretisation](#Discretisation)
+  - [Matrix Formulation](#Matrix-Formulation)
+  - [Solving Linear Systems](#Solving-Linear-Systems)
+  - [Convergence](#Convergence)
+- [CFD](#CFD)
+  - [One-Dimension](#One-Dimension)
+    - [Linear Convection](#Linear-Convection)
+    - [Non-Linear Convection](#Non-Linear-Convection)
+    - [Diffusion](#Diffusion)
+    - [Burgers' Equation](#Burgers'-Equation)
+  - [Two-Dimension](#Two-Dimension)
+    - [Linear Convection](#Linear-Convection)
+    - [Non-Linear Convection](#Non-Linear-Convection)
+    - [Diffusion](#Diffusion)
+    - [Burgers' Equation](#Burgers'-Equation)
+    - [Laplace Equation](#Laplace-Equation)
+    - [Poisson Equation](#Poisson-Equation)
+  - [Navier-Stokes Equations in 2D](#Navier-Stokes-Equations-in-2D)
+    - [Lid Driven Cavity](#Lid-Driven-Cavity)
--- main.cpp
+++ main.cpp
@@ -23,9 +23,157 @@ double u[Nx][Ny]; // x-velocity
 double v[Nx][Ny]; // y-velocity
 double p[Nx][Ny]; // Pressure
 
-
+void initialize();
+void apply_boundary_conditions();
+void solve_navier_stokes();
 
 int main() {
-    cout << "Hello World!" << endl;
+    initialize();
+
+    // Main simulation loop
+    for (int time_step = 0; time_step < 2; ++time_step) {
+        apply_boundary_conditions();
+        solve_navier_stokes();
+    
+        // Print out u, v, p
+        cout << "Time step: " << time_step << endl;
+        cout << "u:" << endl;
+        for (int j = 0; j < Ny; ++j) {
+            for (int i = 0; i < Nx; ++i) {
+                cout << u[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << "v:" << endl;
+        for (int j = 0; j < Ny; ++j) {
+            for (int i = 0; i < Nx; ++i) {
+                cout << v[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << "p:" << endl;
+        for (int j = 0; j < Ny; ++j) {
+            for (int i = 0; i < Nx; ++i) {
+                cout << p[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+    }
+
     return 0;
+}
+
+void initialize() {
+    // Initialize u, v, p
+    for (int i = 0; i < Nx; i++) {
+        for (int j = 0; j < Ny; j++) {
+            u[i][j] = 1.0;
+            v[i][j] = 1.0;
+            p[i][j] = 1.0;
+        }
+    }
+}
+
+void apply_boundary_conditions() {
+    // Apply boundary conditions to u, v, p
+
+    // Left boundary
+    for (int j = 0; j < Ny; ++j) {
+        u[0][j] = 0.0;
+        v[0][j] = 0.0;
+        p[0][j] = p[1][j];
+    }
+
+    // Right boundary
+    for (int j = 0; j < Ny; ++j) {
+        u[Nx-1][j] = 0.0;
+        v[Nx-1][j] = 0.0;
+        p[Nx-1][j] = p[Nx-2][j];
+    }
+
+    // Bottom boundary
+    for (int i = 0; i < Nx; ++i) {
+        u[i][0] = 0.0;
+        v[i][0] = 0.0;
+        p[i][0] = p[i][1];
+    }
+
+    // Top boundary
+    for (int i = 0; i < Nx; ++i) {
+        u[i][Ny-1] = U;
+        v[i][Ny-1] = 0.0;
+        p[i][Ny-1] = p[i][Ny-2];
+    }
+
+}
+
+void solve_navier_stokes() {
+    // Solve Navier-Stokes equations for u, v, p
+
+    // Temporary arrays to store intermediate velocities
+    double u_temp[Nx][Ny];
+    double v_temp[Nx][Ny];
+
+    // Update u and v using the discretized Navier-Stokes equations
+    for (int i = 1; i < Nx - 1; ++i) {
+        for (int j = 1; j < Ny - 1; ++j) {
+            u_temp[i][j] = u[i][j] + dt * (
+                -u[i][j] * (u[i][j] - u[i-1][j]) / dx
+                - v[i][j] * (u[i][j] - u[i][j-1]) / dy
+                - (1.0 / rho) * (p[i+1][j] - p[i][j]) / dx
+                + mu * (
+                    (u[i+1][j] - 2.0 * u[i][j] + u[i-1][j]) / (dx * dx)
+                    + (u[i][j+1] - 2.0 * u[i][j] + u[i][j-1]) / (dy * dy)
+                )
+            );
+
+            v_temp[i][j] = v[i][j] + dt * (
+                -u[i][j] * (v[i][j] - v[i-1][j]) / dx
+                - v[i][j] * (v[i][j] - v[i][j-1]) / dy
+                - (1.0 / rho) * (p[i][j+1] - p[i][j]) / dy
+                + mu * (
+                    (v[i+1][j] - 2.0 * v[i][j] + v[i-1][j]) / (dx * dx)
+                    + (v[i][j+1] - 2.0 * v[i][j] + v[i][j-1]) / (dy * dy)
+                )
+            );
+        }
+    }
+
+    // Solve the pressure Poisson equation
+    // (Here, we use a simple iterative method, e.g., the Jacobi method)
+    for (int iter = 0; iter < maxIterations; ++iter) {
+        double p_temp[Nx][Ny];
+
+        // Update pressure field
+        for (int i = 1; i < Nx - 1; ++i) {
+            for (int j = 1; j < Ny - 1; ++j) {
+                p_temp[i][j] = 0.25 * (
+                    p[i+1][j] + p[i-1][j] + p[i][j+1] + p[i][j-1]
+                    - dx * dy * rho / (2.0 * dt) * (
+                        (u_temp[i+1][j] - u_temp[i-1][j]) / (2.0 * dx)
+                        + (v_temp[i][j+1] - v_temp[i][j-1]) / (2.0 * dy)
+                    )
+                );
+            }
+        }
+
+        // Update pressure field
+        for (int i = 1; i < Nx - 1; ++i) {
+            for (int j = 1; j < Ny - 1; ++j) {
+                p[i][j] = p_temp[i][j];
+            }
+        }
+    }
+
+    // Update velocities using the pressure gradient
+    for (int i = 1; i < Nx - 1; ++i) {
+        for (int j = 1; j < Ny - 1; ++j) {
+            u[i][j] = u_temp[i][j] - (dt / dx) * (p[i+1][j] - p[i][j]);
+            v[i][j] = v_temp[i][j] - (dt / dy) * (p[i][j+1] - p[i][j]);
+        }
+    }
+
 }
